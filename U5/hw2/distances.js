20a1c4c86929f3a1345e9c0c47985c98980b4101
ginsuown <ethanwang350@gmail.com>
2018-07-19 18:20:20 -0700

    Finished hw2-Distances

--- U5/hw2/distances.js
+++ U5/hw2/distances.js
@@ -1,7 +1,57 @@
-// Read Synchrously
-var fs = require("fs");
-console.log(process.cwd());
-console.log("\n *START* \n");
-var content = fs.readFileSync(__dirname + "\\coordinates.json");
-console.log("Output Content : \n" + content);
-console.log("\n *EXIT* \n");
+//load data from coordinates.json
+let fs = require("fs");
+let path = require("path");
+let content = fs.readFileSync(path.join(__dirname, "coordinates.json"));
+let data = JSON.parse(content);
+console.log(`${data.length} point(s) loaded from coordinates.json`);
+
+function processCoordinates(input, data) {
+  data.forEach(element => {
+    point = element.value.split(",");
+    element.distance = Math.sqrt(
+      Math.pow(input[0] - point[0], 2) + Math.pow(input[1] - point[1], 2)
+    );
+  });
+  data.sort((a, b) => {
+    return a.distance - b.distance;
+  });
+  //output result
+  data.forEach(a => {
+    console.log(`{ id: '${a.id}', value: '${a.value}'`);
+  });
+}
+
+function checkInput(line) {
+  let input = line.split(",");
+  if (input.length !== 2) {
+    console.log(`2 arguments required, ${input.length} were entered.`);
+    return false;
+  }
+  for (let i of input) {
+    if (Number.parseInt(i) != i) {
+      console.log(`Arguments must be integers.`);
+      return false;
+    }
+  }
+  return input;
+}
+
+function prompt() {
+  let readline = require("readline");
+  let rl = readline.createInterface(process.stdin, process.stdout);
+  rl.setPrompt("Please specify origin point x(0-100), y(0-100)> ");
+  rl.prompt();
+  rl.on("line", function(line) {
+    let input = checkInput(line);
+    if (input) {
+      processCoordinates(input, data);
+      rl.close();
+    }
+    rl.prompt();
+  }).on("close", function() {
+    // process coordinates
+    process.exit(0);
+  });
+}
+
+prompt();
