b33354eb058c9e3c741388eb3c4f40a1e998a33f
ginsuown <ethanwang350@gmail.com>
2018-07-25 12:23:46 -0700

    Finished U7-hw1, installed request module

--- U7/hw1.js
+++ U7/hw1.js
@@ -1,3 +1,23 @@
-function myRequest() {
-  //
+var request = require("request");
+
+function myRequest(url) {
+  let p = new Promise((resolve, reject) => {
+    request(url, function(error, response, body) {
+      if (!error) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
+  });
+  return p;
 }
+
+let req = myRequest("https://www.google.com");
+
+req.then(response => {
+  console.log(response);
+});
+req.catch(error => {
+  console.log(error);
+});
