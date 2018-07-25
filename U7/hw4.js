d89ff8a0db91b1e61472850c6b3a437c0b3862e2
ginsuown <ethanwang350@gmail.com>
2018-07-24 21:49:51 -0700

    Finished hw4-missing ranges checker

---
+++ U7/hw4.js
@@ -0,0 +1,36 @@
+// Given a sorted integer array, return the missing ranges
+function missingRanges(inputs, lower, upper) {
+  let results = [];
+  //check lower bounds
+  if (inputs[0] - lower > 1) {
+    results.push(lower.toString() + "->" + (inputs[i] - 1).toString());
+  }
+  if (inputs[0] === lower + 1) {
+    results.push(lower.toString());
+  }
+  //check middle
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i + 1] - inputs[i] === 2) {
+      results.push((inputs[i] + 1).toString());
+    }
+    if (inputs[i + 1] - inputs[i] > 2) {
+      results.push(
+        (inputs[i] + 1).toString() + "->" + (inputs[i + 1] - 1).toString()
+      );
+    }
+  }
+  //check upper bounds
+  if (upper - inputs[inputs.length - 1] > 1) {
+    results.push(
+      (inputs[inputs.length - 1] + 1).toString() + "->" + upper.toString()
+    );
+  }
+  if (inputs[inputs.length - 1] === upper - 1) {
+    results.push(upper.toString());
+  }
+  return results;
+}
+
+//test
+let input = [0, 1, 3, 50, 75];
+console.log(missingRanges(input, 0, 99));
